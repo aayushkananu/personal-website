29f38b5723dac4e59527511b4e405e647966c77b
aayushkananu <112506495+aayushkananu@users.noreply.github.com>
2023-06-19 17:35:30 -0400

    Add files via upload
    first commit

---
+++ mobile.js
@@ -0,0 +1,13 @@
+const text = "lowercase";
+const typing = document.getElementById("typing");
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    typing.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
+  }
+}
+
+typeWriter();
